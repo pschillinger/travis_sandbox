d1d521d0bbe7f3e6203b409dcbfdc69617b84d92
Philipp Schillinger <philsplus@gmail.com>
2018-09-07 23:08:07 +0200

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -3,7 +3,7 @@
 export ROS_PARALLEL_JOBS='-j8'
 export ROSINSTALL_FILE='~/flexbe_ci/workspace.rosinstall'
 
-alias flexbe_ci_before_install='~/flexbe_ci/ci_scripts/before_install.bash'
-alias flexbe_ci_install='~/flexbe_ci/ci_scripts/install.bash'
-alias flexbe_ci_before_script='~/flexbe_ci/ci_scripts/before_script.bash'
-alias flexbe_ci_script='~/flexbe_ci/ci_scripts/script.bash'
+alias flexbe_ci_before_install='. ~/flexbe_ci/ci_scripts/before_install.bash'
+alias flexbe_ci_install='. ~/flexbe_ci/ci_scripts/install.bash'
+alias flexbe_ci_before_script='. ~/flexbe_ci/ci_scripts/before_script.bash'
+alias flexbe_ci_script='. ~/flexbe_ci/ci_scripts/script.bash'
