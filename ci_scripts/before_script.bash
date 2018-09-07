34bc2e582ec1df040e802ce5f6aea242227c90d0
Philipp Schillinger <philsplus@gmail.com>
2018-09-08 00:06:30 +0200

    Remove env var for rosinstall file

--- ci_scripts/before_script.bash
+++ ci_scripts/before_script.bash
@@ -4,7 +4,7 @@ set -e
 # clone packages
 cd ~/catkin_ws/src
 wstool init
-wstool merge $ROSINSTALL_FILE
+wstool merge ~/flexbe_ci/workspace.rosinstall
 wstool up
 cd ~/catkin_ws
 rosdep install -y --from-paths src --ignore-src
--- setup.bash
+++ setup.bash
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 export ROS_PARALLEL_JOBS='-j8'
-export ROSINSTALL_FILE='~/flexbe_ci/workspace.rosinstall'
 export ROS_DISTRO=${1:-indigo}
 
 chmod -R +x ~/flexbe_ci/ci_scripts
