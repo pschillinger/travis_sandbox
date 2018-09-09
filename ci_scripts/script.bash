ed13b03343d56f9b8b9ee76def1f01cbf5296596
Philipp Schillinger <philsplus@gmail.com>
2018-09-09 18:39:33 +0200

    Add flexbe_app tests

--- ci_scripts/script.bash
+++ ci_scripts/script.bash
@@ -15,17 +15,15 @@ rosrun flexbe_widget create_repo test
 # TEST run ros tests
 cd ~/catkin_ws
 catkin build
+xvfb-run rosrun flexbe_app run_app --offline --run-tests
 catkin run_tests && catkin_test_results
-
-# TEST run flexbe_app devel tests
-# xvfb-run roslaunch ~/flexbe_ci/launch/test_flexbe_devel.launch
 
 # install packages
 cd ~/catkin_ws
 catkin clean --yes
 catkin config --install
 catkin build
+rm -rf src/
 source ~/catkin_ws/install/setup.bash
-
-# TEST run flexbe_app install tests
-# xvfb-run roslaunch ~/flexbe_ci/launch/flexbe_install.launch
+xvfb-run rosrun flexbe_app run_app --offline --run-tests
+catkin run_tests && catkin_test_results
