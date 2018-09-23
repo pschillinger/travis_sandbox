f0c3a9b0fb92b30a7a2a900e4f1c8d894f37c9a1
Philipp Schillinger <philsplus@gmail.com>
2018-09-23 15:00:44 +0200

    Separate rosinstall files to include current repo

--- ci_scripts/before_script.bash
+++ ci_scripts/before_script.bash
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-# clone packages
+# clone required packages
 cd ~/catkin_ws/src
 wstool init
-wstool merge ~/flexbe_ci/workspace.rosinstall
+for rosinstall in ~/flexbe_ci/rosinstall/*.rosinstall; do
+    if [ "$(basename $rosinstall .rosinstall)" -ne "$BASE_REPO" ]; then
+        wstool merge $rosinstall
+    else
+        ln -s $BASE_PATH ~/$BASE_REPO
+    fi
+done
 wstool up
 cd ~/catkin_ws
 rosdep install -y --from-paths src --ignore-src
--- setup.bash
+++ setup.bash
@@ -2,5 +2,8 @@
 
 export ROS_PARALLEL_JOBS='-j8'
 export ROS_DISTRO=${1:-indigo}
-
+export BASE_PATH=$PWD
+export BASE_REPO=${PWD##*/}
 chmod -R +x ~/flexbe_ci/ci_scripts
+
+echo "Running build for $BASE_REPO"
